23d5b16af47684a6e5293abad54cf95e28f5a9d9
huabenwuxin <xiejunliang2019@gmail.com>
2024-10-25 17:27:37 +0800

    添加刷新token和校验token的接口

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from .views import LoginView, RegistrView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
+    # 登录
     path('login/', LoginView.as_view()),
+    # 注册
     path('register/', RegistrView.as_view()),
-
+    # 刷新token
+    path('token/refresh/', TokenRefreshView.as_view()),
+    # 校验token
+    path('token/verify/', TokenVerifyView.as_view())
 ]
